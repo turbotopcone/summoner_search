27ea9a068beb671588286407bddf669386b89f63
turbotopcone <65316569+turbotopcone@users.noreply.github.com>
2020-05-14 00:40:38 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace LOL_Find_Summoners
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.NA_radio = new MetroFramework.Controls.MetroRadioButton();
+            this.EUW_radio = new MetroFramework.Controls.MetroRadioButton();
+            this.EUNE_radio = new MetroFramework.Controls.MetroRadioButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // metroTextBox1
+            // 
+            this.metroTextBox1.Location = new System.Drawing.Point(45, 157);
+            this.metroTextBox1.Multiline = true;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.Size = new System.Drawing.Size(675, 193);
+            this.metroTextBox1.Style = MetroFramework.MetroColorStyle.Silver;
+            this.metroTextBox1.TabIndex = 0;
+            this.metroTextBox1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroTextBox1.UseStyleColors = true;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel1.Location = new System.Drawing.Point(144, 76);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(467, 25);
+            this.metroLabel1.Style = MetroFramework.MetroColorStyle.Silver;
+            this.metroLabel1.TabIndex = 1;
+            this.metroLabel1.Text = "Enter the text from champ select (with joined the lobby part)";
+            this.metroLabel1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.metroButton1.Location = new System.Drawing.Point(193, 365);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(105, 40);
+            this.metroButton1.Style = MetroFramework.MetroColorStyle.Silver;
+            this.metroButton1.TabIndex = 2;
+            this.metroButton1.Text = "Check OP.GG";
+            this.metroButton1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel2.Location = new System.Drawing.Point(0, 392);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(151, 25);
+            this.metroLabel2.Style = MetroFramework.MetroColorStyle.Silver;
+            this.metroLabel2.TabIndex = 3;
+            this.metroLabel2.Text = "conebog123#4303";
+            this.metroLabel2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroLabel2.UseStyleColors = true;
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel3.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.metroLabel3.Location = new System.Drawing.Point(736, 7);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(24, 25);
+            this.metroLabel3.Style = MetroFramework.MetroColorStyle.Silver;
+            this.metroLabel3.TabIndex = 4;
+            this.metroLabel3.Text = "X";
+            this.metroLabel3.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroLabel3.Click += new System.EventHandler(this.metroLabel3_Click);
+            // 
+            // NA_radio
+            // 
+            this.NA_radio.AutoSize = true;
+            this.NA_radio.Location = new System.Drawing.Point(286, 118);
+            this.NA_radio.Name = "NA_radio";
+            this.NA_radio.Size = new System.Drawing.Size(40, 15);
+            this.NA_radio.Style = MetroFramework.MetroColorStyle.Silver;
+            this.NA_radio.TabIndex = 5;
+            this.NA_radio.TabStop = true;
+            this.NA_radio.Text = "NA";
+            this.NA_radio.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.NA_radio.UseStyleColors = true;
+            this.NA_radio.UseVisualStyleBackColor = true;
+            // 
+            // EUW_radio
+            // 
+            this.EUW_radio.AutoSize = true;
+            this.EUW_radio.Location = new System.Drawing.Point(353, 118);
+            this.EUW_radio.Name = "EUW_radio";
+            this.EUW_radio.Size = new System.Drawing.Size(48, 15);
+            this.EUW_radio.Style = MetroFramework.MetroColorStyle.Silver;
+            this.EUW_radio.TabIndex = 6;
+            this.EUW_radio.TabStop = true;
+            this.EUW_radio.Text = "EUW";
+            this.EUW_radio.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.EUW_radio.UseStyleColors = true;
+            this.EUW_radio.UseVisualStyleBackColor = true;
+            // 
+            // EUNE_radio
+            // 
+            this.EUNE_radio.AutoSize = true;
+            this.EUNE_radio.Location = new System.Drawing.Point(427, 118);
+            this.EUNE_radio.Name = "EUNE_radio";
+            this.EUNE_radio.Size = new System.Drawing.Size(52, 15);
+            this.EUNE_radio.Style = MetroFramework.MetroColorStyle.Silver;
+            this.EUNE_radio.TabIndex = 7;
+            this.EUNE_radio.TabStop = true;
+            this.EUNE_radio.Text = "EUNE";
+            this.EUNE_radio.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.EUNE_radio.UseStyleColors = true;
+            this.EUNE_radio.UseVisualStyleBackColor = true;
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.metroButton2.Location = new System.Drawing.Point(462, 365);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(117, 40);
+            this.metroButton2.Style = MetroFramework.MetroColorStyle.Silver;
+            this.metroButton2.TabIndex = 8;
+            this.metroButton2.Text = "Check Porofessor.gg";
+            this.metroButton2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(766, 419);
+            this.ControlBox = false;
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.EUNE_radio);
+            this.Controls.Add(this.EUW_radio);
+            this.Controls.Add(this.NA_radio);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.metroTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Silver;
+            this.Text = "Summoner Searcher";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroRadioButton NA_radio;
+        private MetroFramework.Controls.MetroRadioButton EUW_radio;
+        private MetroFramework.Controls.MetroRadioButton EUNE_radio;
+        private MetroFramework.Controls.MetroButton metroButton2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using MetroFramework.Forms;
+using MetroFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LOL_Find_Summoners
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void metroLabel3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        public string selected_radio_button()
+        {
+            if (NA_radio.Checked) return "na";
+            else if (EUW_radio.Checked) return "euw";
+            else if (EUNE_radio.Checked) return "eune";
+            else return "NOTHING_SELECTED";
+        }
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            var radio_val = selected_radio_button();
+
+            if (radio_val == "NOTHING_SELECTED")
+            {
+                MessageBox.Show("Region not selected!","Error: Region not selected");
+                return;
+            }
+            string[] linije = metroTextBox1.Text.Split('\n');
+
+            foreach (string linija in linije)
+            {
+                string summ_name = linija.Replace(" joined the lobby", "");
+                string url = "https://" + radio_val + ".op.gg/summoner/userName=" + summ_name;
+                Process.Start(url);
+            }
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            var radio_val = selected_radio_button();
+
+            if (radio_val == "NOTHING_SELECTED")
+            {
+                MessageBox.Show("Region not selected!", "Error: Region not selected");
+                return;
+            }
+
+            string[] linije = metroTextBox1.Text.Split('\n');
+
+            foreach (string linija in linije)
+            {
+                string summ_name = linija.Replace(" joined the lobby", "");
+                string url = "https://porofessor.gg/live/" + radio_val + "/" + summ_name;
+                Process.Start(url);
+                break;
+            }
+        }
+    }
+}
